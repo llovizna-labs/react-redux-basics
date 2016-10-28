9406536137a5a0ec165bd5bbe59e45b48e9a3dd0
Ronald Suez <rsuez93@gmail.com>
2016-10-28 08:19:24 -0430

    added a simple reducer

--- src/js/client.js
+++ src/js/client.js
@@ -2,6 +2,57 @@ import React from "react";
 import ReactDom from "react-dom";
 import Layout from "./components/Layout";
 
-const app = document.getElementById('app');
+import {
+  createStore,
+  applyMiddleware
+} from 'redux';
 
-ReactDom.render(<Layout/>, app);
+import createLogger from 'redux-logger';
+
+const logger = createLogger();
+
+
+const initialState = {
+  result: 1,
+  lastValues: []
+}
+
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD':
+      state = {
+        ...state,
+        lastValues: [].concat(state.lastValues, state.result),
+        result: state.result + action.payload
+      }
+      break;
+    case 'SUBSTRACT':
+      state = {
+        ...state,
+        lastValues: [].concat(state.lastValues, state.result),
+        result: state.result - action.payload
+      }
+      break;
+  }
+
+  return state;
+}
+
+const store = createStore(reducer, applyMiddleware(logger));
+
+
+store.dispatch({
+  type: 'ADD',
+  payload: 10
+});
+
+
+store.dispatch({
+  type: 'SUBSTRACT',
+  payload: 2
+});
+
+// const app = document.getElementById('app');
+//
+// ReactDom.render( < Layout / > , app);
